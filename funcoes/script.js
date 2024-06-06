561aee5da2ca3a385ffb5a125de7b0c29dab32eb
gabriel.dslima <gabriel.dslima@dataprev.gov.br>
2024-06-06 19:57:15 -0300

    finalizada a parte de exercicios envolvendo funções na aula de (funções 3)

--- funcoes/script.js
+++ funcoes/script.js
@@ -8,12 +8,12 @@
 */
 
 function areaQuadrado(lado) {
-    return lado * lado;
+  return lado * lado;
 }
 
-areaQuadrado(4) // 16
-areaQuadrado(5) // 25
-areaQuadrado(2) // 4
+areaQuadrado(4); // 16
+areaQuadrado(5); // 25
+areaQuadrado(2); // 4
 
 /*
 
@@ -26,12 +26,12 @@ areaQuadrado(2) // 4
 
 // peso e altura são os parâmetros
 function imc(peso, altura) {
-    const imc = peso / (altura ** 2);
-    return imc;
+  const imc = peso / altura ** 2;
+  return imc;
 }
 
-imc(80, 1.80) // 80 e 1.80 são os argumentos
-imc(60, 1.70) // 60 e 1.70 são os argumentos
+imc(80, 1.8); // 80 e 1.80 são os argumentos
+imc(60, 1.7); // 60 e 1.70 são os argumentos
 
 /* 
 
@@ -41,13 +41,13 @@ imc(60, 1.70) // 60 e 1.70 são os argumentos
 */
 
 function corFavorita(cor) {
-    if (cor === "azul") {
-        return "Eu gosto do céu"
-    } else if (cor === "verde") {
-        return "Eu gosto de mato"
-    } else {
-        return "Eu não gosto de nada"
-    }
+  if (cor === "azul") {
+    return "Eu gosto do céu";
+  } else if (cor === "verde") {
+    return "Eu gosto de mato";
+  } else {
+    return "Eu não gosto de nada";
+  }
 }
 
 /*
@@ -59,8 +59,8 @@ function corFavorita(cor) {
 
 */
 
-addEventListener('click', function () {
-    console.log('Clicou');
+addEventListener("click", function () {
+  console.log("Clicou");
 });
 // A função possui dois argumentos
 // Primeiro é a string 'click'
@@ -78,12 +78,12 @@ addEventListener('click', function () {
 */
 
 function imc2(peso, altura) {
-    const imc = peso / (altura ** 2);
-    console.log(imc);
+  const imc = peso / altura ** 2;
+  console.log(imc);
 }
 
-imc2(80, 1.80); // retorna o imc
-console.log(imc2(80, 1.80)); // retorna o imc e undefined
+imc2(80, 1.8); // retorna o imc
+console.log(imc2(80, 1.8)); // retorna o imc e undefined
 
 /*
  
@@ -94,13 +94,13 @@ Uma função pode retornar qualquer tipo de dado e até outras funções.
 */
 
 function terceiraIdade(idade) {
-    if (typeof idade !== 'number') {
-        return 'Informe a sua idade!';
-    } else if (idade >= 60) {
-        return true;
-    } else {
-        return false;
-    }
+  if (typeof idade !== "number") {
+    return "Informe a sua idade!";
+  } else if (idade >= 60) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /*
@@ -111,18 +111,84 @@ function terceiraIdade(idade) {
  
 */
 
-var profissao = 'Designer';
+var profissao = "Designer";
 
 function dados() {
-    var nome = 'André';
-    var idade = 28;
-    function outrosDados() {
-        var endereco = 'Rio de Janeiro';
-        var idade = 29;
-        return `${nome}, ${idade}, ${endereco}, ${profissao}`;
-    }
-    return outrosDados();
+  var nome = "André";
+  var idade = 28;
+  function outrosDados() {
+    var endereco = "Rio de Janeiro";
+    var idade = 29;
+    return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+  }
+  return outrosDados();
 }
 
 dados(); // Retorna 'André, 29, Rio de Janeiro, Designer'
-outrosDados(); // retorna um erro
+// outrosDados(); // retorna um erro
+
+// ----------- EXERCICIO ---------------- //
+
+// Crie uma função para verificar se um valor é Truthy
+
+    function itsTrue (dado) {
+        return Boolean(dado)
+    }
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+function perimetroQuadrado(lado) {
+  return lado * 4;
+}
+
+console.log(perimetroQuadrado(5));
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+function nomeSobrenome(nome, sobrenome) {
+  return nome + " " + sobrenome;
+}
+
+console.log(nomeSobrenome("Gabriel", "Lima"));
+
+// Crie uma função que verifica se um número é par
+
+function verificaPar(numero) {
+  if (numero % 2 === 0) {
+    return "é par";
+  } else if (numero % 2 !== 0) {
+    return "não é par";
+  } else {
+    return "Digite um valor válido";
+  }
+}
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+function tipoArgumento(arg) {
+  return typeof arg;
+}
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+
+addEventListener("scroll", () => {
+  console.log("Gabriel Lima");
+});
+
+// Corrija o erro abaixo
+let totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+precisoVisitar(20);
+jaVisitei(20);
